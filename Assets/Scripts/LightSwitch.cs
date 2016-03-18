804cf65956a51c97b23ad338a2a4a9da2e44e3d7
federuiz <ruizfederico95@gmail.com>
2016-03-18 16:12:19 -0300

    lights on

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+	public GameObject[] lights;
+	private bool lightsOn = true;
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag == "Player") {
+			Debug.Log ("Lights!!");
+			for (int i = 0; i < lights.Length; i++) {
+				lights [i].GetComponent<Light> ().enabled = !lightsOn;
+			}
+			lightsOn = !lightsOn;
+		}
+	}
+}
