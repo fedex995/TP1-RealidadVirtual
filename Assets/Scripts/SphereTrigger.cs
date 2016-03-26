60d0312daf509b51a58f7855c9b920b695a4e093
federuiz <ruizfederico95@gmail.com>
2016-03-26 16:48:13 -0300

    bouncing spheres and some chairs

---
+++ Assets/Scripts/SphereTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereTrigger : MonoBehaviour {
+
+	public Rigidbody[] spheres;
+	public Vector3 Gravity;
+	private bool isTriggered;
+
+	void OnTriggerEnter(Collider c) {
+		if (c.tag == "Player") {
+			for (int i = 0; i < spheres.Length; i++) {
+				spheres [i].useGravity = true;
+			}
+		}
+	}
+}
